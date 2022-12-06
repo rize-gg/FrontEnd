43938c8057aaeba7113cacd9003d32f8249f82dc
Arman <armani.developer@gmail.com>
2022-12-06 15:51:53 +0100

    validation on empty input

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 document.getElementById('trapezoidParent').addEventListener("click", function(e){
     const email = document.getElementById('email');
-    fetch('waitlist.php/?email='+email.value, {
-        method: 'GET'
-    })
-    email.value = '';
-    alert("Thank you. We will send you the notification ASAP!")
+    if(email.value !== '') {
+        fetch('waitlist.php/?email='+email.value, {
+            method: 'GET'
+        })
+        email.value = '';
+        alert("Thank you. We will send you the notification ASAP!")
+    }
 })
